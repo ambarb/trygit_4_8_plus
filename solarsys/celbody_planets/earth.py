cadf16ae88ee0dd3e1e32c50af918de87604fdce
Andi <andi@LPS-159615.home>
2020-06-13 17:44:59 -0400

    planet creation modules

---
+++ solarsys/celbody_planets/earth.py
@@ -0,0 +1,9 @@
+def make():
+    """Function returns key parameters about a specific planet which is the outer most key name of the returned dictionary."""
+    data = {}
+    data.update({'earth' : {'description': 'Planet with 20% O2 with 75% of surface covered by H2O. Humans inhabitants enjoy both of these aspects.',
+                            'order' : 1,
+                            'type': 'planet',
+                           }})
+    
+    return data
---
+++ solarsys/celbody_planets/saturn.py
@@ -0,0 +1,9 @@
+def make():
+    """Function returns key parameters about a specific planet which is the outer most key name of the returned dictionary."""
+    data = {}
+    data.update({'saturn' : {'description': 'in our solar system' ,
+                            'order' : 6,
+                            'type': 'planet',
+                           }})
+    
+    return data
